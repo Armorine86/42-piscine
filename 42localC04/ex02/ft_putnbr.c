82629342184bec70fb06d3034cb07a794d7de954
mmondell <mmondell@c2r5p1.42quebec.com>
2021-03-22 17:07:41 -0400

    C04 up to base numbers

---
+++ 42localC04/ex02/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmondell <mmondell@student.42quebec>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/22 09:29:03 by mmondell          #+#    #+#             */
+/*   Updated: 2021/03/22 09:34:27 by mmondell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+		return ;
+	}
+	if (nb < 10 && nb > 0)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
