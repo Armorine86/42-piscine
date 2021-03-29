46c54482be0210806afb099d8f4222b60ff26e5c
armorine86 <armorine1986@gmail.com>
2021-03-28 21:49:31 -0400

    C08

---
+++ 42localC08/ex02/main.c
@@ -0,0 +1,14 @@
+#include "ft_abs.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	int	index;
+
+	index = -5;
+	while (index < 5)
+	{
+		printf("macros::abs(%d) = %d\n", index, ABS(index));
+		index++;
+    }
+}
